f43459d1bff3f00d217ed239e0e9942346a50ed4
Joe Feocco <joefeocco@gmail.com>
2015-08-10 01:30:37 -0400

    Updated runtime function. The script is now functional. Must be run through python. Need to figure out executable next. Maybe extra configurations.

--- rocketResX.py
+++ rocketResX.py
@@ -1,23 +1,23 @@
 class RocketResX(object):
 	"""Change the string 'ResX=1920' in $myDocuments_Location$\My Games\Rocket League\TAGame\Config\TASystemSettings.ini to 'ResX=3840'."""
-	def __init__(self, MyDocuments):
+	def __init__(self):
 		super(RocketResX, self).__init__()
 		# Edit the value below this line to your My Documents directory
 		# Leave the quotation marks and the letter r prefix. DO NOT REMOVE
-		MyDocuments = r'C:\Users\Joe\Documents'
+		self.MyDocuments = r'C:\Users\Joe\Documents'
 
 		# Only edit if the My Games folder isn't in the My Documents directory. 
-		FilePathSuffix = r'\My Games\Rocket League\TAGame\Config\TASystemSettings.ini'
+		self.FilePathSuffix = r'\My Games\Rocket League\TAGame\Config\TASystemSettings.ini'
 		
-		ConfigFilePath = MyDocuments + FilePathSuffix
-		Monitors1 = 'ResX=1920'
-		Monitors2 = 'ResX=3840'
+		self.ConfigFilePath = self.MyDocuments + self.FilePathSuffix
+		self.Monitors1 = 'ResX=1920'
+		self.Monitors2 = 'ResX=3840'
 		
 
 	def replace(self, FilePath, pattern, subst):
 		FileContents = open(FilePath).read()
 		if pattern in FileContents:
-			print('Changing "{pattern}" to "{subst}"'.format(pattern, subst))
+			print('Changing "{0}" to "{1}"'.format(pattern, subst))
 			FileContents = FileContents.replace(pattern, subst)
 			f = open(FilePath, 'w')
 			f.write(FileContents)
@@ -28,7 +28,7 @@ class RocketResX(object):
 
 
 	def runtime(self):
-		inp = raw_input('Press 1 for 1 Monitor.\nPress 2 for 2 Monitors.')
+		inp = input('Press 1 for 1 Monitor.\nPress 2 for 2 Monitors.')
 		if inp == '1':
 			self.replace(self.ConfigFilePath, self.Monitors2, self.Monitors1)
 		else:
